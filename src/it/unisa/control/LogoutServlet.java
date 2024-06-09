af3f7f69ed73e03715167eca208a821182f03655
Miry0 <m.desantis40@studenti.unisa.it>
2024-06-09 20:54:34 +0200

    id-6 unresolved

--- src/it/unisa/control/LogoutServlet.java
+++ src/it/unisa/control/LogoutServlet.java
@@ -23,7 +23,7 @@ public class LogoutServlet extends HttpServlet {
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		
-		doGet(request, response);
+		doGet(request, response);;
 	}
 
 }
